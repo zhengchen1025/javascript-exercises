cb2aaac806d5c0562667da2cb302a7798d074f53
zhengchen1025 <zhengchen1025@gmail.com>
2024-05-05 22:55:31 +0800

    12 done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    
+    for (let obj of arr) {
+        if (!obj.yearOfDeath) {
+            obj.yearOfDeath = new Date().getFullYear();
+        }
+        obj.age = (obj.yearOfDeath - obj.yearOfBirth)
+    }
+    console.log(arr)
+    arr.sort((a, b) => b.age - a.age)
+    console.log(arr)
+        return arr[0]
 };
 
 // Do not edit below this line
