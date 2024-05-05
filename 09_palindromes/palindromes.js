8d53fef70e70c84be8c989fa2e42410e8f6afc33
zhengchen1025 <zhengchen1025@gmail.com>
2024-05-05 22:10:24 +0800

    now 09 added

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let newStr = str.toLowerCase()
+    .replace(/[^a-zA-Z0-9]/g, '')
+    
 
+    for (let i = 0; i < newStr.length; i++) {
+        if (newStr[i] !== newStr[newStr.length - i - 1]) {
+            return false;
+        }
+    }
+    return true
 };
 
 // Do not edit below this line
