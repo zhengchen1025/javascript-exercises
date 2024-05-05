9494f6ada7bc2c82eb19479bdd2b56abf0e97798
zhengchen1025 <zhengchen1025@gmail.com>
2024-05-05 22:08:37 +0800

    09 and 10 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(idx) {
+    let fibo = [1, 1];
+    if (typeof(idx) === 'string') {
+        return fibonacci(parseInt(idx))
+    }
+    if (idx === 0 ) {
+        return 0
+    }
+    else if (idx < 0) {
+        return 'OOPS'
+    }
+    else {
+        
+    for (let i = 2; i < idx + 1; i++) {
+            fibo.push(fibo[i - 2] + fibo[i - 1])
+        }
+    }
+    console.log(fibo)
+    return fibo[idx-1];
 };
 
 // Do not edit below this line
